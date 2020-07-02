ea315776ff91650b53985988abff26a411c39681
Nazorm <nnadoziechinazor@gmail.com>
2020-07-02 15:52:22 +0100

    js

--- trafficlight.js
+++ trafficlight.js
@@ -5,7 +5,6 @@ var z = document.querySelector(".green");
 var redVar = setInterval(changeRed, 3000);
 var stopRed = setInterval(clearRed, 6000);
 
-
 var orangeVar = setInterval(changeOrange, 6000);
 var stopOrange = setInterval(clearOrange, 9000);
 
@@ -14,36 +13,49 @@ var stopGreen = setInterval(clearGreen, 10000)
 
 
 
+
+
+
+
+
+
+
 function changeRed() {
-    x.style.backgroundColor = x.style.backgroundColor  == "red" ? "red" : "red";
-    x.style.color = x.style.color == "white" ? "white" : "white";
+    x.style.backgroundColor  = "red";
+    x.style.color = "white";
+    clearOrange() ;
+    clearGreen();
 }
 function clearRed(){
-   x.style.backgroundColor = x.style.backgroundColor  == "transparent" ? "transparent" : "transparent";
-    x.style.color = x.style.color == "transparent" ? "transparent" : "transparent";
+    x.style.backgroundColor  = "transparent";
+    x.style.color = "transparent";
 }
+
 
 
 
 function changeOrange() { 
-    y.style.backgroundColor = y.style.backgroundColor  == "orange" ? "orange" : "orange";
-    y.style.color = y.style.color == "white" ? "white" : "white";
+    y.style.backgroundColor  = "orange";
+    y.style.color = "white";
+    clearRed();
+    clearGreen()
 }
 function clearOrange() {
-    y.style.backgroundColor = y.style.backgroundColor  == "transparent" ? "transparent" : "transparent";
-    y.style.color = y.style.color == "transparent" ? "transparent" : "transparent";
+    y.style.backgroundColor  = "transparent";
+    y.style.color = "transparent";
+    
 }
-
 
 
 function changeGreen(){
-    z.style.backgroundColor = z.style.backgroundColor == "green" ? "green" : "green";
-    z.style.color = z.style.color == "white" ? "white" : "white";
+    z.style.backgroundColor = "green";
+    z.style.color = "white";
+    clearRed();
+    clearOrange() 
 }
 function clearGreen(){
-    
-   z.style.backgroundColor = z.style.backgroundColor == "transparent" ? "transparent" : "transparent";
-   z.style.color = z.style.color == "transparent" ? "transparent" : "transparent";
+   z.style.backgroundColor = "transparent";
+   z.style.color = "transparent";
 }
 
 
